d37a51a264e4510f10123da64bb15abaa3b38ca5
maniacinahuel <nahuelmaniaci@gmail.com>
2022-09-02 01:15:43 -0300

    Conexion mongo

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 
-const FileStore = require('session-file-store')(session)
-
+//const FileStore = require('session-file-store')(session)
+const MongoStore = require('connect-mongo')
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true}
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
 
 app.use(session({
-    store: new FileStore({path: './sesiones', ttl: 60}),
+    //store: new FileStore({path: './sesiones', ttl: 60}),
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://Nahuel_Maniaci:nahue123@cluster0.qvotb9b.mongodb.net/test',
+        mongoOptions: advancedOptions
+    }),
+    
     secret: 'secreto',
     resave: false,
     saveUninitialized: false
@@ -28,26 +34,27 @@ app.use(session({
     }
     console.log(nombre, valor, tiempo);
     res.json({proceso: "OK"})
-}) */
-
-/* app.get('/cookies', (req, res) => {
+}) 
+app.get('/cookies', (req, res) => {
     console.log(req.cookies)
     res.json({normales: req.cookies, firmadas: req.signedCookies})
 }) */
 
-app.get('/cookiesSessions', (req, res) => {
+app.post('/cookiesSessions', (req, res) => {
+    let { name } = req.body
+    req.session.name = name
     if(req.session.contador){
         req.session.contador++
         res.send(`Contador: ${req.session.contador}`)
     } else {
         req.session.contador = 1
-        res.send(`BIENVENIDO`)
+        res.send(`BIENVENIDO ${req.session.name}`)
     }
    /*  res.json({normales: req.cookies, firmadas: req.signedCookies}) */
 })
 
 
-app.get('clearCookies', (req, res) => {
+app.get('/clearCookies', (req, res) => {
     req.session.destroy( err => {
         if (!err) res.send("Cleared!")
         else res.send({status: "Not cleared - ERROR", body: err})
